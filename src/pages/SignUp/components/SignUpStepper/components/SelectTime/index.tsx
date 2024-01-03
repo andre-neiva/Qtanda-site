d2f3b22e0ce74bd0383a2d8446b4fc8faf5b5b0f
andre-neiva <110946719+andre-neiva@users.noreply.github.com>
2024-01-03 20:56:07 -0300

    feat: adicionar tela de seleção de horário de funcionamento

--- src/pages/SignUp/components/SignUpStepper/components/SelectTime/index.tsx
+++ src/pages/SignUp/components/SignUpStepper/components/SelectTime/index.tsx
@@ -1,7 +1,83 @@
-export default function SelectTime() {
+import React, { useState } from 'react';
+import './style.css';
+
+const daysOfWeek = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+
+interface Time {
+  day: string;
+  start: string;
+  end: string;
+  secondStart: string;
+  secondEnd: string;
+}
+
+function App() {
+  const [times, setTimes] = useState<Time[]>(
+    daysOfWeek.map((day) => ({
+      day,
+      start: '',
+      end: '',
+      secondStart: '',
+      secondEnd: '',
+    }))
+  );
+
+  const handleInputChange = (day: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    setTimes(
+      times.map((time) =>
+        time.day === day
+          ? {
+              ...time,
+              [event.target.name]: event.target.value,
+            }
+          : time
+      )
+    );
+  };
+
   return (
-    <div>
-      <p>Select Time</p>
+    <div className="container">
+      {times.map((time) => (
+        <div className="card" key={time.day}>
+          <h2>{time.day}</h2>
+          <label className="label-text">
+            Aberto das:
+            <p>
+              <input
+                type="time"
+                name="start"
+                value={time.start}
+                onChange={(event) => handleInputChange(time.day, event)}
+              />{' '}
+              até as
+              <input type="time" name="end" value={time.end} onChange={(event) => handleInputChange(time.day, event)} />
+            </p>
+          </label>
+          <label className="label-text">
+            e também das:
+            <p>
+              <input
+                type="time"
+                name="secondStart"
+                value={time.secondStart}
+                onChange={(event) => handleInputChange(time.day, event)}
+              />{' '}
+              até as
+              <input
+                type="time"
+                name="secondEnd"
+                value={time.secondEnd}
+                onChange={(event) => handleInputChange(time.day, event)}
+              />
+            </p>
+          </label>
+        </div>
+      ))}
+      <div className="optional-text">
+        <p>*O segundo horário é opcional</p>
+      </div>
     </div>
   );
 }
+
+export default App;
